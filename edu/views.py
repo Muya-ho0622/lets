93c32ae6e57884c24241f30caf49a63dcf030eca
jiho0622 <pjgcoolboy4u@gmail.com>
2023-12-09 11:48:28 +0900

    이미지 업로드

--- edu/views.py
+++ edu/views.py
@@ -1,7 +1,9 @@
 
 # Create your views here.
 from django.views.generic import View
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from.models import Feed #현재폴더에 models 라는 파일에서 
+#Feed 라는 이름의 class를 가져오세요.
 
 class Index(View):
     template_name = 'index.html'
@@ -14,3 +16,18 @@ class TagStudy(View):
 
     def get(self, request):
         return render(request, self.template_name)
+    
+class Newcontent(View):
+    templete_name = "upload_form.html"
+    
+    def get(self, request):
+        return render(request, self.templete_name)
+    def post(self, request):
+        param = request.POST.get('content','')
+        param2 = request.FILES.get('up_photo', False)
+        
+        print(f"param{param}")
+        feed = Feed(content = param, photo = param2)
+        feed.save()
+        return redirect('edu:tag_study')
+    
